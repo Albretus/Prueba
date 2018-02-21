35c6f8d5d601bea2eeab2c6361704c312da54cc7
Albretus <36697490+Albretus@users.noreply.github.com>
2018-02-21 12:55:53 +0100

    Update JavaApplication4.java

--- JavaApplication4/src/javaapplication4/JavaApplication4.java
+++ JavaApplication4/src/javaapplication4/JavaApplication4.java
@@ -17,7 +17,7 @@ public class JavaApplication4 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Hello Git");
+        System.out.println("Hello Gitteros como estais hoy que lo quiero saber yo");
     }
     
 }
